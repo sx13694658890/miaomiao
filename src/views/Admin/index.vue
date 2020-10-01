7a95968645b5f56bda886faee369e0d5e4a148fe
sxl <sxl13694658890@163.com>
2020-10-01 08:02:42 +0800

    我们的操作  add   createComponent

---
+++ src/views/Admin/index.vue
@@ -0,0 +1,46 @@
+<template>
+  
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name : 'admin',
+  
+}
+</script>
+
+<style scoped>
+.el-header, .el-footer {
+background-color: #B3C0D1;
+color: #333;
+padding-left:60px;
+line-height: 60px;
+}
+
+.el-aside {
+background-color: #D3DCE6;
+color: #333;
+text-align: center;
+line-height: 200px;
+}
+
+.el-main {
+background-color: #E9EEF3;
+color: #333;
+text-align: center;
+}
+
+body > .el-container {
+margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+line-height: 320px;
+}
+</style>
